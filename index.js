38e68bf9a5894c4384a8a17cbff3937b4ba2933d
Amenov <amenov.abdulsalam@gmail.com>
2021-02-21 18:56:57 +0600

    M 1856 21022021

--- index.js
+++ index.js
@@ -1,33 +1,112 @@
 const redis = require('redis');
 
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST ?? '127.0.0.1',
-  port: process.env.REDIS_PORT ?? '6379',
-});
-
-const redisPrefix = process.env.REDIS_PREFIX ?? '';
-
-// SET
-const redisSet = (key, payload) => {
-  redisClient.set(redisPrefix + key, payload);
-};
-
-// GET
-const redisGet = (key) => {
-  return new Promise((resolve, reject) => {
-    redisClient.get(redisPrefix + key, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
+/*
+  options: Object
+    - redisClient?: RedisClient
+    - redisHost?: String
+    - redisPort?: String
+    - redisPrefix?: String
+*/
+module.exports = (options = {}) => {
+  const redisClient =
+    options.redisClient ??
+    redis.createClient({
+      host: options.redisHost ?? process.env.REDIS_HOST ?? '127.0.0.1',
+      port: options.redisPort ?? process.env.REDIS_PORT ?? '6379',
     });
-  });
-};
 
-// DEL
-const redisDel = (key) => {
-  redisClient.del(redisPrefix + key);
-};
+  /*
+    @ADD-PREFIX
+  
+    key: String
+  */
+  const addPrefix = (key) => {
+    const prefix = options.redisPrefix ?? process.env.REDIS_PREFIX;
 
-module.exports = { redisClient, redisSet, redisGet, redisDel };
+    return (prefix ? prefix + '_' : '') + key;
+  };
+
+  /*
+    @CREATE-KEY
+    
+    key: String | Array:String
+    withPrefix: Boolean
+  */
+  const createKey = (key, withPrefix = true) => {
+    if (withPrefix) {
+      if (Array.isArray(key)) {
+        return key.map((key) => addPrefix(key));
+      } else {
+        addPrefix(key);
+      }
+    } else {
+      return key;
+    }
+  };
+
+  /*
+    @GET
+    
+    key: String
+    withPrefix?: Boolean
+  */
+  const redisGet = (key, withPrefix) => {
+    return new Promise((resolve, reject) => {
+      redisClient.get(createKey(key, withPrefix), (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  };
+
+  /*
+    @SET
+    
+    key: String
+    payload: String
+    withPrefix?: Boolean
+  */
+  const redisSet = (key, payload, withPrefix) => {
+    redisClient.set(createKey(key, withPrefix), payload);
+  };
+
+  /*
+    @KEYS
+
+    key: String
+    withPrefix?: Boolean
+  */
+  const redisKeys = (key, withPrefix) => {
+    return new Promise((resolve, reject) => {
+      redisClient.keys(createKey(key, withPrefix), (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  };
+
+  /*
+    @DEL
+
+    key: String | Array:String
+    withPrefix?: Boolean
+  */
+  const redisDel = (key, withPrefix) => {
+    redisClient.del(createKey(key, withPrefix));
+  };
+
+  return {
+    redisClient,
+    createKey,
+    redisSet,
+    redisGet,
+    redisKeys,
+    redisDel,
+  };
+};
